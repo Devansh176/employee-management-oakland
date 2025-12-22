9958dfdddc865c14e558424a5859dddc756609c5
Devansh <devanshdhopte@gmail.com>
2025-12-22 16:17:48 +0530

    First commit as intern of oakland

---
+++ src/main/java/employee/model/Employee.java
@@ -0,0 +1,46 @@
+package employee.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "dob")
+    private LocalDate dob;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "login_id", unique = true)
+    private String login_id;
+
+    @Column(name = "password")
+    private String password;
+}
